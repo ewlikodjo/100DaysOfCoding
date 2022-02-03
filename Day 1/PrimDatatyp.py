43f404a72dbecfa9ea6fe28df65d1f9f2efcbba2
gato <kodjoewli@gmail.com>
2022-02-03 15:23:28 +0530

    this is data type in python

---
+++ Day 1/PrimDatatyp.py
@@ -0,0 +1,18 @@
+# Datatype
+# String
+
+print("Hello!"[3])
+
+print("542" + "345")
+
+# Integer
+
+print(123 + 344)
+# the computer consider 33342_453423_32 like an integer
+print(type(3_13_233))
+
+# float (4532.5; 243.34 ...)
+print(type(34.5))
+
+# boolean (true and false)
+
