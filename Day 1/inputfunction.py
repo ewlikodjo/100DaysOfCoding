1fa0663310cfc78f3e7dbbbb965382d179faf2ae
gato <kodjoewli@gmail.com>
2022-02-03 15:43:27 +0530

    string concatenation

---
+++ Day 1/inputfunction.py
@@ -0,0 +1,9 @@
+#function declaration
+two_digit_number = input("Type two digits numbers:")
+
+first = two_digit_number[0]
+second = two_digit_number[1]
+
+result = first + second
+
+print(type(result))
