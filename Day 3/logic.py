1a5b7cde6cbfe01499b628788d462e9e6897b0f3
gato <kodjoewli@gmail.com>
2022-02-13 17:33:46 +0530

    leap year checking multipl we

---
+++ Day 3/logic.py
@@ -0,0 +1,9 @@
+age = int(input("enter your age: "))
+height = int(input("enter your height: "))
+
+if age <= 12:
+    if height > 100:
+    print("you are eligible")
+else:
+    print("hmm you are the way")
+    print("you are old")
