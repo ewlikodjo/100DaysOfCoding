4bbf88213b2ca978f5f97eb2312f4b182619a401
gato <kodjoewli@gmail.com>
2022-02-17 10:31:03 +0530

    love score by python list update

--- Day 4/love_score.py
+++ Day 4/love_score.py
@@ -1,9 +1,9 @@
 import random
 
-randomIntegeter = random.randint(1, 10)
-print(randomIntegeter)
+randomInteger = random.randint(1, 10)
+print(randomInteger)
 
-randomFloat = random.random() * 5
+randomFloat = random.random * 5
 print(randomFloat)
 
 love_score = random.randint(1, 100)
