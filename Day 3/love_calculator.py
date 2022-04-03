567d184ed0af9a2fa01c80d0fda03676be8bfc13
gato <kodjoewli@gmail.com>
2022-04-03 12:17:31 +0530

    this is dictionary prin in python v

--- Day 3/love_calculator.py
+++ Day 3/love_calculator.py
@@ -23,12 +23,11 @@ love_score = str(true) + str(love)
 
 print(love_score)
 
-love_score  = int(str(true) + str(love))
+love_score = int(str(true) + str(love))
 
 if (love_score < 10) or (love_score > 90):
     print(f"Your love score is {love_score}, you go together like coke and mentor")
 elif (love_score >= 40) and (love_score <= 50):
-    print(f"Your score is {love_score}, you are alright togother")
+    print(f"Your score is {love_score}, you are alright together")
 else:
     print(f"Your score is {love_score}")
-
